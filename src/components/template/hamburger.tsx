ab67c02734bf3af5375830300d246c6b35bd77b9
Chasel <arko9988b@gmail.com>
2023-05-29 03:05:02 +0800

    style navbar item space and border

--- src/components/template/hamburger.tsx
+++ src/components/template/hamburger.tsx
@@ -14,13 +14,13 @@ const Hamburger = () => {
           <Disclosure.Button className="pt-1">
             {!open ? <Bars3Icon className='w-5 h-5'/> : <XMarkIcon className='w-5 h-5'/>}
           </Disclosure.Button>
-          <Disclosure.Panel as="ul" className="flex flex-col gap-4 fixed top-12 right-0 w-1/2 h-full p-5 bg-slate-200">
+          <Disclosure.Panel as="ul" className="flex flex-col fixed top-12 right-0 w-1/2 h-full p-3 bg-slate-200 border-l border-gray-300">
             {routes.filter((route) => !route.index).map(route => (  
-              <li key={route.label} className='w-full flex flex-col gap-3'>
-                <Disclosure.Button as={Link} href={route.path} className="w-full text-xl pl-2">
+              <li key={route.label}>
+                <Disclosure.Button as={Link} href={route.path} className="flex items-center pl-2 py-3 hover:bg-slate-300 active:bg-slate-400 text-xl">
                   {route.label}
                 </Disclosure.Button>
-                <div className=' border-b-2 border-gray-300'/>
+                <div className='border-b border-gray-400'/>
               </li>
             ))}
           </Disclosure.Panel>
--- src/components/template/navigation.tsx
+++ src/components/template/navigation.tsx
@@ -7,17 +7,17 @@ const Navigation = () => {
   // Idea: combine navbar and toggleNavbar together, like
   // <nav>...</nav><nav>...</nav> -> <nav>...</nav>
   return (
-    <header className="sticky top-0 w-full bg-slate-200 flex justify-between items-center h-12 px-5 sm:h-16 border-b-2 border-gray-200">
+    <header className="sticky top-0 w-full bg-slate-200 flex justify-between items-center h-12 px-5 sm:h-16 border-b border-gray-300">
       <h1>
         {routes.filter((route) => route.index).map(route => (
-          <Link href={route.path} key={route.label} className="text-xl lg:text-2xl">{route.label}</Link>
+          <Link href={route.path} key={route.label} className="text-xl">{route.label}</Link>
         ))}
       </h1>
       <nav className="hidden sm:block sm:h-full sm:py-2">
         <ul className="flex h-full">
           {routes.filter((route) => !route.index).map(route => (
-            <li key={route.label} className="h-full hover:bg-slate-300 sm:rounded-lg">
-                <Link href={route.path} className="text-lg lg:text-2xl px-2 h-full flex items-center">{route.label}</Link>
+            <li key={route.label} className="h-full hover:bg-slate-300 active:bg-slate-400 sm:rounded-lg">
+                <Link href={route.path} className="flex items-center h-full px-2 text-lg">{route.label}</Link>
             </li>
           ))}
         </ul>
