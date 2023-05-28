bfd510b4a57ed05052c110addafbcc07b5d6bb5a
Chasel <arko9988b@gmail.com>
2023-05-28 20:49:38 +0800

    finish navbar

--- src/components/template/hamburger.tsx
+++ src/components/template/hamburger.tsx
@@ -6,19 +6,21 @@ import routes from "@/data/routes";
 import Link from 'next/link';
 
 const Hamburger = () => {
+  // Need add animation to icon and panel
   return (
     <Disclosure as='nav' className="sm:hidden">
       {({ open }) => (
         <>
-          <Disclosure.Button className="py-2">
-            {!open ? <Bars3Icon className='w-6'/> : <XMarkIcon className='w-6'/>}
+          <Disclosure.Button className="pt-1">
+            {!open ? <Bars3Icon className='w-5 h-5'/> : <XMarkIcon className='w-5 h-5'/>}
           </Disclosure.Button>
-          <Disclosure.Panel as="ul">
-            {routes.filter((route) => !route.index).map(route => (            
-              <li key={route.label}>
-                <Disclosure.Button as={Link} href={route.path}>
+          <Disclosure.Panel as="ul" className="flex flex-col gap-4 fixed top-12 right-0 w-1/2 h-full p-5 bg-slate-200">
+            {routes.filter((route) => !route.index).map(route => (  
+              <li key={route.label} className='w-full flex flex-col gap-3'>
+                <Disclosure.Button as={Link} href={route.path} className="w-full text-xl pl-2">
                   {route.label}
                 </Disclosure.Button>
+                <div className=' border-b-2 border-gray-300'/>
               </li>
             ))}
           </Disclosure.Panel>
--- src/components/template/navigation.tsx
+++ src/components/template/navigation.tsx
@@ -7,17 +7,17 @@ const Navigation = () => {
   // Idea: combine navbar and toggleNavbar together, like
   // <nav>...</nav><nav>...</nav> -> <nav>...</nav>
   return (
-    <header className="bg-slate-200 flex justify-between p-5">
+    <header className="sticky top-0 w-full bg-slate-200 flex justify-between items-center h-12 px-5 sm:h-16 border-b-2 border-gray-200">
       <h1>
         {routes.filter((route) => route.index).map(route => (
-          <Link href={route.path} key={route.label}>{route.label}</Link>
+          <Link href={route.path} key={route.label} className="text-xl lg:text-2xl">{route.label}</Link>
         ))}
       </h1>
-      <nav className="hidden sm:block">
-        <ul className="flex gap-5">
+      <nav className="hidden sm:block sm:h-full sm:py-2">
+        <ul className="flex h-full">
           {routes.filter((route) => !route.index).map(route => (
-            <li key={route.label}>
-              <Link href={route.path}>{route.label}</Link>
+            <li key={route.label} className="h-full hover:bg-slate-300 sm:rounded-lg">
+                <Link href={route.path} className="text-lg lg:text-2xl px-2 h-full flex items-center">{route.label}</Link>
             </li>
           ))}
         </ul>
